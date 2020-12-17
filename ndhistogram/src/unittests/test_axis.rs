9660113cae3c9a3ad87da9d36d341e14203decd5
David Hadley <davehadley@users.noreply.github.com>
2020-12-17 18:33:04 +0000

    Add a test of uniform.

--- ndhistogram/src/unittests/test_axis.rs
+++ ndhistogram/src/unittests/test_axis.rs
@@ -1,3 +1,5 @@
+use std::vec;
+
 use crate::axis::Axis;
 use crate::axis::Uniform;
 
@@ -10,4 +12,17 @@ fn test_uniform_numbins() {
         high: 1.0,
     };
     assert_eq!(ax.numbins(), nbins)
+}
+
+#[test]
+fn test_uniform_index() {
+    let nbins = 5;
+    let ax = Uniform {
+        num: nbins,
+        low: 0.0,
+        high: 5.0,
+    };
+    let actual: Vec<usize> = (-2..7).map(|it| ax.index(&(it as f64))).collect();
+    let expected = vec![0, 0, 1, 2, 3, 4, 5, 6, 6];
+    assert_eq!(expected, actual)
 }
