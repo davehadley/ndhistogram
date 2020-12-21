151e0741b4e259547dcf9a4e44317b3cf624594f
David Hadley <davehadley@users.noreply.github.com>
2020-12-21 05:04:00 +0000

    Test passes.

--- ndhistogram/src/macros.rs
+++ ndhistogram/src/macros.rs
@@ -10,4 +10,14 @@ macro_rules! ndhistogram {
             $crate::histogram::ArrayHistogram::<_, $t>::new(axes)
         }
     };
+    ( $( $x:expr ),+ ) => {
+        {
+            let axes = (
+            $(
+                $x,
+            )*
+        );
+            $crate::histogram::ArrayHistogram::<_, f64>::new(axes)
+        }
+    };
 }
--- ndhistogram/src/unittests/test_histogram_1d.rs
+++ ndhistogram/src/unittests/test_histogram_1d.rs
@@ -1,3 +1,5 @@
+use crate::axis::Uniform;
+
 #[test]
 fn test_histogram_1d_uniform_constructor() {
     let hist = ndhistogram!(Uniform::new(5, 0.0, 1.0));
