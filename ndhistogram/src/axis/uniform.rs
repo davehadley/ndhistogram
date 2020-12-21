ff70f0cd47010ae8c9d88cfb4a505674f9787ba2
David Hadley <davehadley@users.noreply.github.com>
2020-12-21 03:20:45 +0000

    generalize uniform to all floating point numbers.

--- ndhistogram/src/axis/uniform.rs
+++ ndhistogram/src/axis/uniform.rs
@@ -1,38 +1,42 @@
-use std::ops::Range;
+use num::{Float, FromPrimitive, NumCast};
+use std::{fmt::Debug, ops::Mul, ops::Range, ops::Sub};
 
 use super::Axis;
 
-pub struct Uniform {
+pub struct Uniform<T = f64> {
     num: usize,
-    low: f64,
-    high: f64,
+    low: T,
+    high: T,
 }
 
-impl Uniform {
-    pub fn new(num: usize, low: f64, high: f64) -> Uniform {
+impl<T> Uniform<T>
+where
+    T: PartialOrd + Debug,
+{
+    pub fn new(num: usize, low: T, high: T) -> Uniform<T> {
         if num == 0 {
             panic!("Invalid axis num bins ({})", num);
         }
         if low >= high {
-            panic!("Invalid axis range bins (low:{}, high:{})", low, high);
+            panic!("Invalid axis range bins (low::{:?}, high:{:?})", low, high);
         }
         Uniform { num, low, high }
     }
 }
 
-impl Axis for Uniform {
-    type Coordinate = f64;
+impl<T: Float> Axis for Uniform<T> {
+    type Coordinate = T;
     type BinItem = Range<Self::Coordinate>;
 
     fn index(&self, coordinate: &Self::Coordinate) -> usize {
-        let frac = (coordinate - self.low) / (self.high - self.low);
-        if frac < 0.0 {
+        let frac = (*coordinate - self.low) / (self.high - self.low);
+        if frac < T::zero() {
             return 0;
-        } else if frac >= 1.0 {
+        } else if frac >= T::one() {
             return self.num + 1 as usize;
         }
-        let idx = (self.num as f64) * frac;
-        (idx as usize) + 1
+        let idx: T = T::from(self.num).unwrap() * frac;
+        (idx.to_usize().unwrap()) + 1
     }
 
     fn numbins(&self) -> usize {
@@ -43,8 +47,10 @@ impl Axis for Uniform {
         if index == 0 || (index - 1) >= self.num {
             return None;
         }
-        let start = ((index - 1) as f64) * (self.high - self.low) / (self.num as f64);
-        let end = (index as f64) * (self.high - self.low) / (self.num as f64);
+        // let start = ((index - 1) as f64) * (self.high - self.low) / (self.num as f64);
+        // let end = (index as f64) * (self.high - self.low) / (self.num as f64);
+        let start = (T::from(index - 1)?) * (self.high - self.low) / (T::from(self.num)?);
+        let end = (T::from(index)?) * (self.high - self.low) / (T::from(self.num)?);
         Some(Range { start, end })
     }
 }
