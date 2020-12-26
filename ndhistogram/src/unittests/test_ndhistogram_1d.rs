56220abe95b1f8610d43e000d568b9e33e5195b7
David Hadley <davehadley@users.noreply.github.com>
2020-12-26 12:20:59 +0000

    failing tests

--- ndhistogram/src/unittests/test_ndhistogram_1d.rs
+++ ndhistogram/src/unittests/test_ndhistogram_1d.rs
@@ -18,3 +18,22 @@ fn test_histogram_uniform_1d_unfilled_is_empty() {
     let expected = vec![0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0];
     assert_eq!(expected, actual);
 }
+
+#[test]
+#[allow(clippy::float_cmp)]
+fn test_histogram_uniform_2d_unweighted_fill_once() {
+    let mut hist = ndhistogram!(Uniform::new(5, 0.0, 0.5), Uniform::new(5, 0.5, 1.0));
+    hist.fill(&0.1);
+    let actual = *hist.value(&0.1).unwrap();
+    let expected = 1.0;
+    assert_eq!(expected, actual);
+}
+
+#[test]
+#[allow(clippy::float_cmp)]
+fn test_histogram_uniform_2d_unfilled_is_empty() {
+    let hist = ndhistogram!(Uniform::new(5, 0.0, 0.5));
+    let actual: Vec<f64> = hist.iter_values().copied().collect();
+    let expected = vec![0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0];
+    assert_eq!(expected, actual);
+}
