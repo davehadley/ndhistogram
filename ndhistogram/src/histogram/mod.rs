7cd7b77b39d70e93911955d6ca7320460a315c2b
David Hadley <davehadley@users.noreply.github.com>
2020-12-28 05:00:26 +0000

    iterator returns item.

--- ndhistogram/src/histogram/mod.rs
+++ ndhistogram/src/histogram/mod.rs
@@ -1,4 +1,10 @@
 use crate::axes::Axes;
+
+pub struct Item<'a, T, V> {
+    pub index: usize,
+    pub bin: Option<T>,
+    pub value: &'a V,
+}
 pub trait Histogram<'a, A: Axes + 'a, V: 'a> {
     type Values: Iterator<Item = &'a V>;
 
@@ -11,12 +17,12 @@ pub trait Histogram<'a, A: Axes + 'a, V: 'a> {
     }
 
     fn values(&'a self) -> Self::Values;
-    fn iter(&'a self) -> Box<dyn Iterator<Item = (usize, Option<A::BinRange>, &V)> + 'a> {
-        Box::new(
-            self.axes().items().map(move |(index, binrange)| {
-                (index, binrange, self.value_at_index(index).unwrap())
-            }),
-        )
+    fn iter(&'a self) -> Box<dyn Iterator<Item = Item<'a, A::BinRange, V>> + 'a> {
+        Box::new(self.axes().items().map(move |(index, binrange)| Item {
+            index,
+            bin: binrange,
+            value: self.value_at_index(index).unwrap(),
+        }))
     }
 }
 
