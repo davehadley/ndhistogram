a96cb63c3c62f6a67fe1a6550cd3c503765a2533
David Hadley <davehadley@users.noreply.github.com>
2021-01-05 13:54:12 +0000

    test string

--- ndhistogram/src/unittests/test_category_axis.rs
+++ ndhistogram/src/unittests/test_category_axis.rs
@@ -2,25 +2,16 @@ use crate::axis::binrange::SingleValuedBinRange;
 use crate::axis::{Axis, Category};
 use std::{convert::TryFrom, ops::Range};
 
-fn category_ab() -> (Vec<String>, Category<String>) {
-    let cats: Vec<String> = vec![String::from("A"), String::from("B")];
-    (cats.clone(), Category::new(cats))
-}
-
-fn category_abc() -> (Vec<String>, Category<String>) {
-    let cats: Vec<String> = vec![String::from("A"), String::from("B"), String::from("C")];
-    (cats.clone(), Category::new(cats))
-}
-
 #[test]
 fn test_category_numbins() {
-    let (_, ax) = category_abc();
+    let ax = Category::new(vec!["A", "B", "C"]);
     assert_eq!(ax.numbins(), 3 + 1)
 }
 
 #[test]
 fn test_category_get_index() {
-    let (cats, ax) = category_ab();
+    let cats = vec!["A", "B"];
+    let ax = Category::new(cats.clone());
     let actual: Vec<usize> = cats.iter().map(|c| ax.index(&c).unwrap()).collect();
     let expected = vec![0, 1];
     assert_eq!(expected, actual)
@@ -62,7 +53,7 @@ fn test_category_debug_display() {
 
 #[test]
 fn test_category_display() {
-    let (_, ax) = category_abc();
+    let ax = Category::new(vec!["A", "B", "C"]);
     println!("{}", ax);
     assert_eq!(format!("{}", ax), "{{A}, {B}, {C}, {overflow}}")
 }
@@ -105,4 +96,35 @@ fn test_iter_axis() {
     let expected: Vec<_> = ax.iter().collect();
     let actual: Vec<_> = ax.into_iter().collect();
     assert_eq!(expected, actual);
+}
+
+fn string_category_ab() -> (Vec<String>, Category<String>) {
+    let cats: Vec<String> = vec![String::from("A"), String::from("B")];
+    (cats.clone(), Category::new(cats))
+}
+
+fn string_category_abc() -> (Vec<String>, Category<String>) {
+    let cats: Vec<String> = vec![String::from("A"), String::from("B"), String::from("C")];
+    (cats.clone(), Category::new(cats))
+}
+
+#[test]
+fn test_string_category() {
+    // sanaity check that we get sensible results when category is String type
+    let (cats, category) = string_category_ab();
+    assert_eq!(category.numbins(), cats.len() + 1);
+    assert_eq!(category.indices().collect::<Vec<_>>(), vec![0, 1, 2]);
+    assert_eq!(category.index(&"A".to_string()), Some(0));
+    assert_eq!(
+        category.bin(0),
+        Some(SingleValuedBinRange::new(String::from("A")))
+    );
+    assert_eq!(
+        category.bins().collect::<Vec<_>>(),
+        vec![
+            SingleValuedBinRange::new(String::from("A")),
+            SingleValuedBinRange::new(String::from("B")),
+            SingleValuedBinRange::overflow()
+        ]
+    );
 }
