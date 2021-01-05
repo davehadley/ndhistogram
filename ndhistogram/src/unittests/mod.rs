5361df80322a86cea957ef7d0757cb42bde1e6da
David Hadley <davehadley@users.noreply.github.com>
2021-01-05 14:47:25 +0000

    add a failing test for growable

--- ndhistogram/src/unittests/mod.rs
+++ ndhistogram/src/unittests/mod.rs
@@ -2,8 +2,11 @@ mod test_axes;
 mod test_binrange;
 mod test_category_axis;
 mod test_category_binrange;
+mod test_growable_histogram;
 mod test_ndhistogram_1d;
 mod test_ndhistogram_1d_category;
 mod test_ndhistogram_2d;
 mod test_ndhistogram_macro;
 mod test_uniform_axis;
+
+fn test_growable_histogram() {}
---
+++ ndhistogram/src/unittests/test_growable_histogram.rs
@@ -0,0 +1,21 @@
+use crate::{
+    axis::{binrange::SingleValuedBinRange, Category},
+    histogram::{Fill, Histogram, Item},
+};
+
+#[test]
+fn test_histogram_category_grows() {
+    let mut hist = ndhistogram!(Category::new(vec!["A", "B"]); i32);
+    assert_eq!(hist.value(&"C"), Some(&0));
+    hist.fill(&"C");
+    assert_eq!(hist.value(&"C"), Some(&1));
+    // check every bin value
+    let actual: Vec<_> = hist.iter().collect();
+    let expected = vec![
+        Item::new(0, SingleValuedBinRange::new("A"), &0),
+        Item::new(0, SingleValuedBinRange::new("B"), &0),
+        Item::new(0, SingleValuedBinRange::new("C"), &1),
+        Item::new(0, SingleValuedBinRange::overflow(), &0),
+    ];
+    assert_eq!(expected, actual);
+}
