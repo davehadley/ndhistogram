f80238151e56c79c8e9da7843a7fddf0d98d32b2
David Hadley <davehadley@users.noreply.github.com>
2021-01-07 18:12:39 +0000

    partial implementation of ntuple macros.

--- ndhistogram/src/axes.rs
+++ ndhistogram/src/axes.rs
@@ -2,28 +2,32 @@ use crate::histogram::Grow;
 
 use super::axis::Axis;
 
-pub trait Axes: Axis {}
-
-impl<X: Axis> Axes for (X,) {}
-
-impl<X: Axis> Axis for (X,) {
-    type Coordinate = X::Coordinate;
-    type BinRange = X::BinRange;
-
-    fn index(&self, coordinate: &Self::Coordinate) -> Option<usize> {
-        self.0.index(coordinate)
-    }
-
-    fn numbins(&self) -> usize {
-        self.0.numbins()
-    }
-
-    fn bin(&self, index: usize) -> Option<Self::BinRange> {
-        self.0.bin(index)
-    }
+pub trait Axes: Axis {
+    fn num_dim(&self) -> usize;
 }
 
-impl<X: Axis, Y: Axis> Axes for (X, Y) {}
+// impl<X: Axis> Axis for (X,) {
+//     type Coordinate = X::Coordinate;
+//     type BinRange = X::BinRange;
+
+//     fn index(&self, coordinate: &Self::Coordinate) -> Option<usize> {
+//         self.0.index(coordinate)
+//     }
+
+//     fn numbins(&self) -> usize {
+//         self.0.numbins()
+//     }
+
+//     fn bin(&self, index: usize) -> Option<Self::BinRange> {
+//         self.0.bin(index)
+//     }
+// }
+
+impl<X: Axis, Y: Axis> Axes for (X, Y) {
+    fn num_dim(&self) -> usize {
+        2
+    }
+}
 
 impl<X: Axis, Y: Axis> Axis for (X, Y) {
     type Coordinate = (X::Coordinate, Y::Coordinate);
@@ -64,3 +68,63 @@ impl<X: Axis + Grow<<X as Axis>::Coordinate>, Y: Axis + Grow<<Y as Axis>::Coordi
         Ok(())
     }
 }
+
+// from the little book of rust macros <https://danielkeep.github.io/tlborm/book/blk-counting.html>
+macro_rules! count_idents {
+    ($($idents:ident),* $(,)*) => {
+        {
+            #[allow(dead_code, non_camel_case_types)]
+            enum Idents { $($idents,)* __CountIdentsLast }
+            const COUNT: usize = Idents::__CountIdentsLast as usize;
+            COUNT
+        }
+    };
+}
+
+macro_rules! next_ntuple_impl {
+    ($name:ident, $($other:ident,)*) => (ntuple_impl! { $($other,)* })
+}
+
+macro_rules! ntuple_coordinate {
+    ( $name:ident ) => { <$name as Axis>::Coordinate };
+    ( $($name:ident),+$(,)* ) => { ($(<$name as Axis>::Coordinate),*) };
+}
+
+macro_rules! ntuple_binrange {
+    ( $name:ident ) => { <$name as Axis>::BinRange };
+    ( $($name:ident),+$(,)* ) => { ($(<$name as Axis>::BinRange),*) };
+}
+
+macro_rules! ntuple_impl {
+    () => ();
+    ( $($name:ident),+$(,)* )  => {
+
+        impl<$($name:Axis),*> Axes for ($($name),* ,) {
+            fn num_dim(&self) -> usize {
+                count_idents!($($name),*)
+            }
+        }
+
+        impl<$($name:Axis),*> Axis for ($($name),* ,) {
+            type Coordinate = ntuple_coordinate!($($name),*);
+            type BinRange = ntuple_binrange!($($name),*);
+
+            fn index(&self, coordinate: &Self::Coordinate) -> Option<usize> {
+                self.0.index(coordinate)
+            }
+
+            fn numbins(&self) -> usize {
+                self.0.numbins()
+            }
+
+            fn bin(&self, index: usize) -> Option<Self::BinRange> {
+                self.0.bin(index)
+            }
+        }
+        next_ntuple_impl!($($name,)+);
+    };
+}
+
+ntuple_impl! {T0,}
+
+//ntuple_impl! {T0, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, }
--- ndhistogram/src/unittests/test_axes.rs
+++ ndhistogram/src/unittests/test_axes.rs
@@ -1,11 +1,27 @@
 use float_cmp::approx_eq;
 
-use crate::axis::{Axis, Uniform};
+use crate::axis::{Axis, Category, Uniform};
 use std::ops::Range;
+
+#[test]
+fn test_axes_1d_numbins() {
+    let x = Uniform::new(5, 0.0, 1.0);
+    let xtuple = (x.clone(),);
+    assert_eq!(xtuple.numbins(), x.numbins())
+}
 
 #[test]
 fn test_axes_2d_numbins() {
     let x = Uniform::new(5, 0.0, 1.0);
     let y = Uniform::new(4, 0.0, 1.0);
-    assert_eq!((x, y).numbins(), (5 + 2) * (4 + 2))
+    let xy = (x.clone(), y.clone());
+    assert_eq!(xy.numbins(), x.numbins() * y.numbins())
 }
+
+// #[test]
+// fn test_axes_3d_numbins() {
+//     let x = Uniform::new(5, 0.0, 1.0);
+//     let y = Uniform::new(4, 0.0, 1.0);
+//     let z = Category::new(vec!["A", "B", "C"]);
+//     assert_eq!((x, y, z).numbins(), x.numbins() * y.numbins() * z.numbins());
+// }
