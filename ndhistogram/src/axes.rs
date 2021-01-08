666ac99987f6757295a1c3b75ee3f12392d3bf6a
David Hadley <davehadley@users.noreply.github.com>
2021-01-08 13:51:12 +0000

    failed attempt at macros.

--- ndhistogram/src/axes.rs
+++ ndhistogram/src/axes.rs
@@ -23,35 +23,35 @@ pub trait Axes: Axis {
 //     }
 // }
 
-impl<X: Axis, Y: Axis> Axes for (X, Y) {
-    fn num_dim(&self) -> usize {
-        2
-    }
-}
+// impl<X: Axis, Y: Axis> Axes for (X, Y) {
+//     fn num_dim(&self) -> usize {
+//         2
+//     }
+// }
 
-impl<X: Axis, Y: Axis> Axis for (X, Y) {
-    type Coordinate = (X::Coordinate, Y::Coordinate);
-    type BinRange = (X::BinRange, Y::BinRange);
+// impl<X: Axis, Y: Axis> Axis for (X, Y) {
+//     type Coordinate = (X::Coordinate, Y::Coordinate);
+//     type BinRange = (X::BinRange, Y::BinRange);
 
-    fn index(&self, coordinate: &Self::Coordinate) -> Option<usize> {
-        let ix = self.0.index(&coordinate.0)?;
-        let iy = self.1.index(&coordinate.1)?;
-        Some(ix + self.0.numbins() * iy)
-    }
+//     fn index(&self, coordinate: &Self::Coordinate) -> Option<usize> {
+//         let ix = self.0.index(&coordinate.0)?;
+//         let iy = self.1.index(&coordinate.1)?;
+//         Some(ix + self.0.numbins() * iy)
+//     }
 
-    fn numbins(&self) -> usize {
-        self.0.numbins() * self.1.numbins()
-    }
+//     fn numbins(&self) -> usize {
+//         self.0.numbins() * self.1.numbins()
+//     }
 
-    fn bin(&self, index: usize) -> Option<Self::BinRange> {
-        let ix = index % self.0.numbins();
-        let iy = index / self.0.numbins();
+//     fn bin(&self, index: usize) -> Option<Self::BinRange> {
+//         let ix = index % self.0.numbins();
+//         let iy = index / self.0.numbins();
 
-        let bx = self.0.bin(ix)?;
-        let by = self.1.bin(iy)?;
-        Some((bx, by))
-    }
-}
+//         let bx = self.0.bin(ix)?;
+//         let by = self.1.bin(iy)?;
+//         Some((bx, by))
+//     }
+// }
 
 impl<X: Axis + Grow<<X as Axis>::Coordinate>> Grow<<Self as Axis>::Coordinate> for (X,) {
     fn grow(&mut self, newcoordinate: &<Self as Axis>::Coordinate) -> Result<(), ()> {
@@ -125,6 +125,6 @@ macro_rules! ntuple_impl {
     };
 }
 
-ntuple_impl! {T0,}
+ntuple_impl! {T0, T1}
 
 //ntuple_impl! {T0, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, }
