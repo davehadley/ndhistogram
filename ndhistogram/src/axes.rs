33b5fa0cc09d368e84e0b869cc8b393c54d18d53
David Hadley <davehadley@users.noreply.github.com>
2021-01-08 14:14:11 +0000

    incrementally replacing axes with macro... tests fail

--- ndhistogram/src/axes.rs
+++ ndhistogram/src/axes.rs
@@ -2,53 +2,80 @@ use crate::histogram::Grow;
 
 use super::axis::Axis;
 
-impl<X: Axis> Axis for (X,) {
-    type Coordinate = X::Coordinate;
-    type BinRange = X::BinRange;
+// impl<X: Axis, Y: Axis> Axis for (X, Y) {
+//     type Coordinate = (X::Coordinate, Y::Coordinate);
+//     type BinRange = (X::BinRange, Y::BinRange);
 
-    fn index(&self, coordinate: &Self::Coordinate) -> Option<usize> {
-        self.0.index(coordinate)
-    }
+//     fn index(&self, coordinate: &Self::Coordinate) -> Option<usize> {
+//         let ix = self.0.index(&coordinate.0)?;
+//         let iy = self.1.index(&coordinate.1)?;
+//         Some(ix + self.0.numbins() * iy)
+//     }
 
-    fn numbins(&self) -> usize {
-        self.0.numbins()
-    }
+//     fn numbins(&self) -> usize {
+//         self.0.numbins() * self.1.numbins()
+//     }
 
-    fn bin(&self, index: usize) -> Option<Self::BinRange> {
-        self.0.bin(index)
-    }
-}
+//     fn bin(&self, index: usize) -> Option<Self::BinRange> {
+//         let ix = index % self.0.numbins();
+//         let iy = index / self.0.numbins();
 
-impl<X: Axis, Y: Axis> Axis for (X, Y) {
-    type Coordinate = (X::Coordinate, Y::Coordinate);
-    type BinRange = (X::BinRange, Y::BinRange);
-
-    fn index(&self, coordinate: &Self::Coordinate) -> Option<usize> {
-        let ix = self.0.index(&coordinate.0)?;
-        let iy = self.1.index(&coordinate.1)?;
-        Some(ix + self.0.numbins() * iy)
-    }
-
-    fn numbins(&self) -> usize {
-        self.0.numbins() * self.1.numbins()
-    }
-
-    fn bin(&self, index: usize) -> Option<Self::BinRange> {
-        let ix = index % self.0.numbins();
-        let iy = index / self.0.numbins();
-
-        let bx = self.0.bin(ix)?;
-        let by = self.1.bin(iy)?;
-        Some((bx, by))
-    }
-}
+//         let bx = self.0.bin(ix)?;
+//         let by = self.1.bin(iy)?;
+//         Some((bx, by))
+//     }
+// }
 
 macro_rules! impl_axes {
     () => {
         pub trait Axes: Axis {}
     };
-    ( ($index:tt => $type_parameter:ident), $( ($nth_index:tt => $nth_type_parameter:ident), )* ) => {
+    ( ($index:tt => $type_parameter:ident), ) => {
+
+        impl<X: Axis> Axes for (X,) {}
+
+        impl<X: Axis> Axis for (X,) {
+            type Coordinate = X::Coordinate;
+            type BinRange = X::BinRange;
+
+            fn index(&self, coordinate: &Self::Coordinate) -> Option<usize> {
+                self.0.index(coordinate)
+            }
+
+            fn numbins(&self) -> usize {
+                self.0.numbins()
+            }
+
+            fn bin(&self, index: usize) -> Option<Self::BinRange> {
+                self.0.bin(index)
+            }
+        }
+
+        impl_axes!();
+    };
+    ( ($index:tt => $type_parameter:ident), $( ($nth_index:tt => $nth_type_parameter:ident), )+ ) => {
         impl<$type_parameter: Axis, $($nth_type_parameter: Axis)*> Axes for ($type_parameter, $($nth_type_parameter)*) {}
+
+        impl<$type_parameter: Axis, $($nth_type_parameter: Axis)*> Axis for ($type_parameter, $($nth_type_parameter)*) {
+            type Coordinate = ($type_parameter::Coordinate, $($nth_type_parameter::Coordinate)*);
+            type BinRange = ($type_parameter::BinRange, $($nth_type_parameter::BinRange)*);
+
+            fn index(&self, coordinate: &Self::Coordinate) -> Option<usize> {
+                //self.0.index(coordinate)
+                todo!()
+            }
+
+            fn numbins(&self) -> usize {
+                //self.0.numbins()
+                todo!()
+            }
+
+            fn bin(&self, index: usize) -> Option<Self::BinRange> {
+                //self.0.bin(index)
+                todo!()
+            }
+        }
+
         impl_axes!($(($nth_index => $nth_type_parameter), )*);
     };
 }
