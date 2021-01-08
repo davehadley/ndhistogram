23549be8d084cf810f1f9233c2dc0b7051200005
David Hadley <davehadley@users.noreply.github.com>
2021-01-08 13:51:42 +0000

    Revert "partial implementation of ntuple macros."
    This reverts commit f80238151e56c79c8e9da7843a7fddf0d98d32b2.

--- ndhistogram/src/unittests/test_axes.rs
+++ ndhistogram/src/unittests/test_axes.rs
@@ -1,27 +1,11 @@
 use float_cmp::approx_eq;
 
-use crate::axis::{Axis, Category, Uniform};
+use crate::axis::{Axis, Uniform};
 use std::ops::Range;
-
-#[test]
-fn test_axes_1d_numbins() {
-    let x = Uniform::new(5, 0.0, 1.0);
-    let xtuple = (x.clone(),);
-    assert_eq!(xtuple.numbins(), x.numbins())
-}
 
 #[test]
 fn test_axes_2d_numbins() {
     let x = Uniform::new(5, 0.0, 1.0);
     let y = Uniform::new(4, 0.0, 1.0);
-    let xy = (x.clone(), y.clone());
-    assert_eq!(xy.numbins(), x.numbins() * y.numbins())
+    assert_eq!((x, y).numbins(), (5 + 2) * (4 + 2))
 }
-
-// #[test]
-// fn test_axes_3d_numbins() {
-//     let x = Uniform::new(5, 0.0, 1.0);
-//     let y = Uniform::new(4, 0.0, 1.0);
-//     let z = Category::new(vec!["A", "B", "C"]);
-//     assert_eq!((x, y, z).numbins(), x.numbins() * y.numbins() * z.numbins());
-// }
