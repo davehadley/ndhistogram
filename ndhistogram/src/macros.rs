635cac8a7437ea8dbb9203a09d8d14bb7b9db389
David Hadley <davehadley@users.noreply.github.com>
2021-01-10 18:15:27 +0000

    Add some examples.

--- ndhistogram/src/macros.rs
+++ ndhistogram/src/macros.rs
@@ -1,3 +1,27 @@
+/// Responsible for creating Histograms.
+///
+/// This macro is the recommended method for constructing new Histograms.
+///
+/// # Example
+///
+/// ## Creating a 1D Histogram
+/// ```rust
+/// use ndhistogram::axis::Uniform;
+/// use std::fmt::Display;
+/// use ndhistogram::{ndhistogram, Histogram};
+///
+/// let hist1D = ndhistogram!(Uniform::new(10, -5.0, 5.0));
+/// println!("{}", hist1D);
+/// ```
+///
+/// ## Creating a 2D Histogram
+/// ```rust
+/// use ndhistogram::axis::Uniform;
+/// use ndhistogram::{ndhistogram, Histogram};
+///
+/// let hist2D = ndhistogram!(Uniform::new(10, -5.0, 5.0), Uniform::new(10, -5.0, 5.0));
+/// println!("{}", hist2D);
+/// ```
 #[macro_export]
 macro_rules! ndhistogram {
 
