ad6170f56f95a8b81f0808dff641c30871d5dbee
David Hadley <davehadley@users.noreply.github.com>
2021-01-10 17:21:58 +0000

    Switch on some warnings.

--- ndhistogram/src/lib.rs
+++ ndhistogram/src/lib.rs
@@ -1,11 +1,19 @@
 #![cfg_attr(
     debug_assertions,
-    warn(missing_debug_implementations, rust_2018_idioms, unreachable_pub, unused_import_braces),
+    warn(
+        missing_debug_implementations,
+        rust_2018_idioms,
+        unreachable_pub,
+        unused_import_braces
+    ),
     deny(unsafe_code, macro_use_extern_crate),
-    //warn(missing_docs, missing_crate_level_docs, missing_doc_code_examples, missing_docs),
+    warn(
+        missing_docs,
+        missing_crate_level_docs,
+        missing_doc_code_examples,
+        missing_docs
+    )
 )]
-
-// TODO: add warning for missing_docs before publishing
 
 mod axes;
 pub mod axis;
