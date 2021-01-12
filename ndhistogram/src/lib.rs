df0b63a00f9a7e304556042a5402aae39c49805d
David Hadley <davehadley@users.noreply.github.com>
2021-01-12 14:26:04 +0000

    Update docs.

--- ndhistogram/src/lib.rs
+++ ndhistogram/src/lib.rs
@@ -6,6 +6,56 @@
 //! - Flexible bin values including any primitive number type, or a user-defined struct.
 //! - Unweighted and weighted filling of histograms.
 //!
+//! ## Quick-start
+//!
+//! ```rust
+//! use ndhistogram::{Histogram, axis::Axis, ndhistogram, axis::Uniform, axis::Category};
+//!
+//! // create a 1D histogram with 10 equally sized bins between -5 and 5
+//! let mut hist = ndhistogram!(Uniform::new(10, -5.0, 5.0));
+//! // fill this histogram with a single value
+//! hist.fill(&1.0);
+//! // fill this histogram with weights
+//! hist.fill_weight(&2.0, 4.0);
+//! // read the histogram values
+//! let x1 = hist.value(&1.0);
+//! let also_x1 = hist.value_at_index(7);
+//! assert_eq!(x1, also_x1);
+//! // iterate the histogram values
+//! for item in hist.iter() {
+//!     println!("{}, {}, {}", item.index, item.bin, item.value)
+//! }
+//! // print the histogram to stdout
+//! println!("{}", hist);
+//!
+//!
+//! // create a 2D histogram
+//! let mut hist = ndhistogram!(Uniform::new(10, -5.0, 5.0), Uniform::new(10, -5.0, 5.0));
+//! // fill 2D histogram
+//! hist.fill(&(1.0, 2.0));
+//! // read back the histogram values
+//! let x1_y2 = hist.value(&(1.0, 2.0));
+//!
+//! // Several axis types are available
+//! let mut hist = ndhistogram!(Category::new(vec!["Red", "Blue", "Green"]));
+//! hist.fill(&"Red");
+//! let red_value = hist.value(&"Red");
+//! // and user axis types may be created by implementing the Axis trait
+//!
+//! // The Histogram bin value type is configurable
+//! let mut hist = ndhistogram!(Uniform::new(10, -5.0, 5.0); i32);
+//! hist.fill_weight(&1.0, 2);
+//! let value: Option<&i32> = hist.value(&1.0);
+//! // and user defined value types are possible by implementing Fill and FillWeight traits
+//!
+//! ```
+//!
+//! ## Overview
+//!
+//! A histogram is composed of two objects:
+//! - An [Axis](crate::axis::Axis) for 1D histograms or set of [Axes] for higher dimensional histograms.
+//! The [Axes] and [Axis](crate::axis::Axis) map from coodinate space (eg \[x,y,z\]) to an integer bin number.
+//! - The histogram bin values. These are any type that implements [Fill] or [FillWeight] (including integer and floating number types).
 
 #![doc(issue_tracker_base_url = "https://github.com/davehadley/rust-hist/issues")]
 #![cfg_attr(
