9f022514e16fea203153e8bf81d3496fc90a0d96
David Hadley <davehadley@users.noreply.github.com>
2021-01-13 17:15:19 +0000

    tests fail

--- ndhistogram/tests/test_variable_axis.rs
+++ ndhistogram/tests/test_variable_axis.rs
@@ -1,6 +1,6 @@
 use std::f64::NAN;
 
-use ndhistogram::axis::{Axis, Variable};
+use ndhistogram::axis::{Axis, BinInterval, Variable};
 
 #[test]
 fn test_variable_numbins() {
@@ -36,4 +36,12 @@ fn test_variable_low() {
 fn test_variable_high() {
     let ax = Variable::new(vec![0, 1, 4, 8]);
     assert_eq!(ax.high(), &8)
+}
+
+#[test]
+fn test_variable_get_index() {
+    let ax = Variable::new(vec![0, 1, 4, 8]);
+    let actual: Vec<_> = (-1..10).map(|coord| ax.index(&coord).unwrap()).collect();
+    let expected = vec![0, 1, 2, 2, 2, 3, 3, 3, 3, 4, 4];
+    assert_eq!(actual, expected);
 }
