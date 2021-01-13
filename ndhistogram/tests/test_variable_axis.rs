197cc0190f0d6afcea39353b8e7df74c96dba17f
David Hadley <davehadley@users.noreply.github.com>
2021-01-13 18:16:33 +0000

    Add more examples.

--- ndhistogram/tests/test_variable_axis.rs
+++ ndhistogram/tests/test_variable_axis.rs
@@ -108,3 +108,51 @@ fn test_variable_iterate_items() {
     let expected: Vec<_> = ax.indices().zip(ax.bins()).collect();
     assert_eq!(actual, expected)
 }
+
+#[test]
+fn test_negative_axis_index() {
+    let axis = Variable::new(vec![-3, -1, 0, 1]);
+    let actual: Vec<_> = (-4..3).map(|loc| axis.index(&loc).unwrap()).collect();
+    let expected = vec![0, 1, 1, 2, 3, 4, 4];
+    assert_eq!(actual, expected)
+}
+
+#[test]
+fn test_negative_axis_bin() {
+    let axis = Variable::new(vec![-3, -1, 0, 1]);
+    let actual: Vec<_> = (0..6).map(|index| axis.bin(index)).collect();
+    let expected = vec![
+        Some(BinInterval::underflow(-3)),
+        Some(BinInterval::new(-3, -1)),
+        Some(BinInterval::new(-1, 0)),
+        Some(BinInterval::new(0, 1)),
+        Some(BinInterval::overflow(1)),
+        None,
+    ];
+    assert_eq!(actual, expected)
+}
+
+#[test]
+fn test_floatingpoint_axis_index() {
+    let axis = Variable::new(vec![-3.0, -1.0, 0.0, 1.0]);
+    let actual: Vec<_> = (-4..3)
+        .map(|loc| axis.index(&(loc as f64)).unwrap())
+        .collect();
+    let expected = vec![0, 1, 1, 2, 3, 4, 4];
+    assert_eq!(actual, expected)
+}
+
+#[test]
+fn test_floatingpoint_axis_bin() {
+    let axis = Variable::new(vec![-3.0, -1.0, 0.0, 1.0]);
+    let actual: Vec<_> = (0..6).map(|index| axis.bin(index)).collect();
+    let expected = vec![
+        Some(BinInterval::underflow(-3.0)),
+        Some(BinInterval::new(-3.0, -1.0)),
+        Some(BinInterval::new(-1.0, 0.0)),
+        Some(BinInterval::new(0.0, 1.0)),
+        Some(BinInterval::overflow(1.0)),
+        None,
+    ];
+    assert_eq!(actual, expected)
+}
