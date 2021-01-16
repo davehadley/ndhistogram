df04d448328b244517f8ade4ff6031cb19d46672
David Hadley <davehadley@users.noreply.github.com>
2021-01-16 11:51:46 +0000

    Test integer mean.

--- ndhistogram/src/value/mean.rs
+++ ndhistogram/src/value/mean.rs
@@ -1,19 +1,28 @@
-use std::ops::{AddAssign, Mul};
+use std::{
+    marker::PhantomData,
+    ops::{AddAssign, Div, Mul},
+};
 
-use num_traits::{abs, Float, Num, NumOps, Signed};
+use num_traits::{abs, Float, Num, NumOps, One, Signed};
 
 use crate::{Fill, FillWith};
 
 use super::Variance;
 
 #[derive(Default, Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Hash, Debug)]
-pub struct Mean<T = f64, C = u32> {
+pub struct Mean<T = f64, C = u32, O = f64> {
     sumw: T,
     sumw2: T,
     count: C,
+    phantom_output_type: PhantomData<O>,
 }
 
-impl<C: Copy, T: Copy + Signed + NumOps + From<C>> Mean<T, C> {
+impl<C, T, O> Mean<T, C, O>
+where
+    O: From<T> + From<C> + NumOps + Signed + Copy,
+    C: Copy,
+    T: Copy,
+{
     pub fn new<I>(values: I) -> Self
     where
         I: IntoIterator<Item = T>,
@@ -24,64 +33,52 @@ impl<C: Copy, T: Copy + Signed + NumOps + From<C>> Mean<T, C> {
         r
     }
 
-    pub fn get(&self) -> T {
+    pub fn get(&self) -> O {
         self.mean()
     }
 
-    pub fn mean(&self) -> T {
-        self.sumw / self.count.into()
+    pub fn mean(&self) -> <O as Div>::Output {
+        O::from(self.sumw) / O::from(self.count)
     }
 
     pub fn num_samples(&self) -> C {
         self.count
     }
 
-    pub fn variance_of_samples(&self) -> T {
+    pub fn variance_of_samples(&self) -> O {
         let mean = self.mean();
         let mean2 = mean * mean;
-        let avgsum2 = self.sumw2 / self.count.into();
+        let avgsum2 = O::from(self.sumw2) / O::from(self.count);
         abs(avgsum2 - mean2)
     }
 
-    pub fn standard_deviation_of_samples(&self) -> T
+    pub fn standard_deviation_of_samples(&self) -> O
     where
-        T: Float,
+        O: Float,
     {
         self.variance_of_samples().sqrt()
     }
 
-    pub fn variance_of_mean(&self) -> T {
-        self.variance_of_samples() / self.count.into()
+    pub fn variance_of_mean(&self) -> O {
+        self.variance_of_samples() / O::from(self.count)
     }
 
-    pub fn standard_error_of_mean(&self) -> T
+    pub fn standard_error_of_mean(&self) -> O
     where
-        T: Float,
+        O: Float,
     {
         self.variance_of_mean().sqrt()
     }
 }
 
-impl<T: Copy> Variance<T> for Mean<T> {
-    fn variance(&self) -> T {
-        self.sumw
-    }
-
-    fn standard_deviation(&self) -> T
-    where
-        T: Float,
-    {
-        self.variance().sqrt()
-    }
-}
-
-impl<T> FillWith<T> for Mean<T>
+impl<T, C, O> FillWith<T> for Mean<T, C, O>
 where
     T: Copy + AddAssign + Mul<Output = T>,
+    C: AddAssign + One,
 {
     fn fill_with(&mut self, data: T) {
         self.sumw += data;
         self.sumw2 += data * data;
-        self.count += 1;
+        self.count += C::one();
     }
 }
--- ndhistogram/tests/test_value_mean.rs
+++ ndhistogram/tests/test_value_mean.rs
@@ -14,7 +14,7 @@ fn assert_float_eq(left: f64, right: f64) {
     )
 }
 
-fn simple_filled_mean_hist() -> VecHistogram<(Uniform,), Mean> {
+fn simple_filled_float_mean_hist() -> VecHistogram<(Uniform,), Mean> {
     let mut hist = ndhistogram!(Uniform::new(1, 0.0, 1.0); Mean);
     hist.fill_with(&0.0, 1.0);
     hist.fill_with(&0.0, 2.0);
@@ -24,20 +24,20 @@ fn simple_filled_mean_hist() -> VecHistogram<(Uniform,), Mean> {
 
 #[test]
 fn test_mean_value_mean() {
-    let hist = simple_filled_mean_hist();
+    let hist = simple_filled_float_mean_hist();
     let binvalue = hist.value(&0.0).unwrap();
     assert_float_eq(hist.value(&0.0).unwrap().get(), 2.0)
 }
 
 #[test]
 fn test_mean_value_numsamples() {
-    let hist = simple_filled_mean_hist();
+    let hist = simple_filled_float_mean_hist();
     let binvalue = hist.value(&0.0).unwrap();
     assert_eq!(binvalue.num_samples(), 3);
 }
 #[test]
 fn test_mean_value_stddev_samples() {
-    let hist = simple_filled_mean_hist();
+    let hist = simple_filled_float_mean_hist();
     let binvalue = hist.value(&0.0).unwrap();
     assert_float_eq(binvalue.variance_of_samples(), 2.0 / 3.0);
     assert_float_eq(
@@ -48,7 +48,42 @@ fn test_mean_value_stddev_samples() {
 
 #[test]
 fn test_mean_value_stderr() {
-    let hist = simple_filled_mean_hist();
+    let hist = simple_filled_float_mean_hist();
+    let binvalue = hist.value(&0.0).unwrap();
+    assert_float_eq(binvalue.variance_of_mean(), (2.0 / 3.0) / 3.0);
+    assert_float_eq(
+        binvalue.standard_error_of_mean(),
+        ((2.0f64 / 3.0) / 3.0).sqrt(),
+    );
+}
+
+fn simple_filled_int_mean_hist() -> VecHistogram<(Uniform,), Mean<i32, i32>> {
+    let mut hist = ndhistogram!(Uniform::new(1, 0.0, 1.0); Mean<i32, i32>);
+    hist.fill_with(&0.0, 1);
+    hist.fill_with(&0.0, 2);
+    hist.fill_with(&0.0, 3);
+    hist
+}
+#[test]
+fn test_mean_value_mean_int() {
+    let hist = simple_filled_float_mean_hist();
+    let binvalue = hist.value(&0.0).unwrap();
+    assert_float_eq(hist.value(&0.0).unwrap().get(), 2.0)
+}
+#[test]
+fn test_mean_int_value_stddev_samples() {
+    let hist = simple_filled_int_mean_hist();
+    let binvalue = hist.value(&0.0).unwrap();
+    assert_float_eq(binvalue.variance_of_samples(), 2.0 / 3.0);
+    assert_float_eq(
+        binvalue.standard_deviation_of_samples() as f64,
+        (2.0f64 / 3.0).sqrt(),
+    );
+}
+
+#[test]
+fn test_mean_int_value_stderr() {
+    let hist = simple_filled_int_mean_hist();
     let binvalue = hist.value(&0.0).unwrap();
     assert_float_eq(binvalue.variance_of_mean(), (2.0 / 3.0) / 3.0);
     assert_float_eq(
