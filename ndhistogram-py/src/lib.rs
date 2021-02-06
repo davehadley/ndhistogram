7eb35172dbd1fb5218418e9ff8c9a46fc176fa9e
David Hadley <davehadley@users.noreply.github.com>
2021-02-06 17:41:17 +0000

    Add a package that contains python bindings.

---
+++ ndhistogram-py/src/lib.rs
@@ -0,0 +1,23 @@
+use pyo3::prelude::*;
+use pyo3::wrap_pyfunction;
+
+#[pyfunction]
+fn hello_world() -> PyResult<String> {
+    Ok("Hello world".to_string())
+}
+
+/// A Python module implemented in Rust.
+#[pymodule]
+fn ndhistogram(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(hello_world, m)?)?;
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
