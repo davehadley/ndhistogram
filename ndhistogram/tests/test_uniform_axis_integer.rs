199c9fd5729d50e5cc8b83bfdf7670d520c678d1
Jacek Generowicz <jacg@my-post-office.net>
2022-01-29 14:35:08 +0100

    Test panic on negative step

--- ndhistogram/tests/test_uniform_axis_integer.rs
+++ ndhistogram/tests/test_uniform_axis_integer.rs
@@ -28,3 +28,9 @@ fn test_uniform_integer_get_bin() {
     ];
     assert_eq!(expected, actual);
 }
+
+#[test]
+#[should_panic]
+fn test_uniform_with_step_size_should_panic_on_negative_step() {
+    Uniform::with_step_size(10, 20.0, -1.0);
+}
