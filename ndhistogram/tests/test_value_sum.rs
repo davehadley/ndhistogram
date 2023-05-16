8d7dcc80c4126df0ec8f359d521e93a09740b45e
rfoxmich <fox@frib.msu.edu>
2023-05-16 11:45:44 -0400

    Support FillWith trait for Sum values (#30)

--- ndhistogram/tests/test_value_sum.rs
+++ ndhistogram/tests/test_value_sum.rs
@@ -1,4 +1,4 @@
-use ndhistogram::{axis::Uniform, ndhistogram, value::Sum, Histogram};
+use ndhistogram::{axis::Uniform, ndhistogram, value::Sum, Histogram, FillWith};
 #[test]
 fn test_sum_value_fill() {
     let mut hist = ndhistogram!(Uniform::new(1, 0.0, 1.0); Sum<i32>);
@@ -19,4 +19,11 @@ fn test_sum_value_error() {
     assert_float_eq(binvalue.get(), 2.0);
     assert_float_eq(binvalue.variance(), 2.0);
     assert_float_eq(binvalue.standard_deviation(), 2.0f64.sqrt());
+}
+
+#[test]
+fn test_sum_value_fill_with() {
+    let mut v = Sum::new();
+    v.fill_with(12345.678);
+    assert_float_eq(v.get(), 12345.678);
 }
