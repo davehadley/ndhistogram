ca1e6db7c0dfa750c19dc97da795462f1b0598f8
David Hadley <davehadley@users.noreply.github.com>
2026-02-14 17:58:00 +0000

    chore: remove unused scripts

--- scripts/install-hooks.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-from pathlib import Path
-from subprocess import run
-
-packagedirectory = Path(__file__).parents[1]
-assert packagedirectory.exists()
-
-run(["pre-commit", "install"], cwd=packagedirectory)
